135787093860eed4e21d351445c437dc45d477a6
mk360 <hamoudi191@hotmail.fr>
2023-06-01 00:19:54 +0200

    add check if assist can be applied

--- dec/assist.d.ts
+++ dec/assist.d.ts
@@ -1,6 +1,11 @@
 import BaseSkill, { BaseSkillArgument } from "./base_skill";
+import Hero from "./hero";
 interface AssistArguments extends BaseSkillArgument {
     range: number;
+    canApplyAssist: ({ wielder, target }: {
+        wielder: Hero;
+        target: Hero;
+    }) => boolean;
 }
 declare class Assist extends BaseSkill {
     constructor(assistConstructor?: AssistArguments);
--- src/assist.ts
+++ src/assist.ts
@@ -1,7 +1,12 @@
 import BaseSkill, { BaseSkillArgument } from "./base_skill";
+import Hero from "./hero";
 
 interface AssistArguments extends BaseSkillArgument {
     range: number;
+    canApplyAssist: ({
+        wielder,
+        target
+    }: { wielder: Hero; target: Hero }) => boolean;
 }
 
 class Assist extends BaseSkill {

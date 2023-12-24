1b08a944554dc0ead6afedb18ae1d597fe2ee6a9
mk360 <n_tonio36.poke@outlook.com>
2023-12-24 12:58:07 +0100

    take reverse affinity into account

--- src/systems/get-affinity.ts
+++ src/systems/get-affinity.ts
@@ -1,10 +1,24 @@
 import { Entity } from "ape-ecs";
+import { WeaponColor } from "../weapon";
 
 function getAffinity(unit1: Entity, unit2: Entity) {
-    const affinityValues: number[] = [];
-    if (unit1.getOne("GuaranteedAffinity")) affinityValues.push(20);
-
-    return Math.max.apply(null, affinityValues);
+    if (unit1.getOne("GuaranteedAffinity")) return 20;
+    const { color: color1 } = unit1.getOne("WeaponType");
+    const { color: color2 } = unit2.getOne("WeaponType");
+    let colorRelationship: "advantage" | "disadvantage" | "neutral" = getColorRelationship(color1, color2);
+    if (unit1.getOne("ReverseAffinity") && colorRelationship === "disadvantage") {
+        colorRelationship = "advantage";
+    }
+    if (unit2.getOne("ReverseAffinity") && colorRelationship === "advantage") {
+        colorRelationship = "disadvantage";
+    }
+    return colorRelationship === "advantage" ? 20 : colorRelationship === "disadvantage" ? -20 : 0;
 };
+
+function getColorRelationship(color1: WeaponColor, color2: WeaponColor) {
+    if ((color1 === "red" && color2 === "blue") || (color1 === "blue" && color2 === "green") || (color1 === "green" && color2 === "red")) return "disadvantage";
+    if ((color1 === "red" && color2 === "green") || (color1 === "blue" && color2 === "red") || (color1 === "green" && color2 === "blue")) return "advantage";
+    return "neutral";
+}
 
 export default getAffinity;

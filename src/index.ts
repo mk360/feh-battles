5c299be9769a86391019e3ffb581576ccb623ec1
mk360 <n_tonio36.poke@outlook.com>
2024-01-08 01:40:32 +0100

    remove OOP files

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-import Hero from "./hero";
-import PassiveSkill from "./passive_skill";
-import Weapon from "./weapon";
-import Combat from "./combat";
-import Team from "./team";
-import MapEffectRunner from "./map-effect";
-import Special from "./special";
-import Assist from "./assist";
-import BattleState from "./battle_state";
 import GameWorld from "./world";
 
-export default {
-    Hero,
-    Special,
-    Assist,
-    PassiveSkill,
-    Team,
-    Weapon,
-    Combat,
-    BattleState,
-    MapEffectRunner,
-    GameWorld
-};
+export default GameWorld;

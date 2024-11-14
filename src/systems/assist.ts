a1b7ac9c694a527f5c18f3b60ccf3f1e631a1052
mk360 <36678834+mk360@users.noreply.github.com>
2024-11-14 12:37:38 +0100

    add assist preview

--- src/systems/assist.ts
+++ src/systems/assist.ts
@@ -2,7 +2,7 @@ import { Entity, Query, System } from "ape-ecs";
 import GameState from "./state";
 import ASSISTS from "../data/assists";
 
-const SUBSCRIBED_COMPONENTS = ["Move", "Heal", "MapBuff", "MapDebuff", "Stats"];
+const SUBSCRIBED_COMPONENTS = ["MapBuff", "MapDebuff", "Stats", "PreviewHP", "Position", "Move", "SacrificeHP"];
 
 class AssistSystem extends System {
     private state: GameState;
@@ -10,7 +10,7 @@ class AssistSystem extends System {
 
     init(state: GameState): void {
         this.state = state;
-        this.query = this.createQuery().fromAll("Assisting");
+        this.query = this.createQuery().fromAny("Assisting", "PreviewAssist");
         for (let component of SUBSCRIBED_COMPONENTS) {
             this.subscribe(component);
         }
@@ -18,18 +18,27 @@ class AssistSystem extends System {
 
     update(): void {
         const [assisting, assisted] = this.query.refresh().execute();
-        const baseAssistingHealth = assisting.getOne("Stats").hp;
-        const baseAssistedHealth = assisted.getOne("Stats").hp;
         const assistSkill = assisting.getOne("Assist");
-        const assistData = ASSISTS[assistSkill.name];
-        if (assistSkill && assistData) {
+        if (assistSkill) {
+            const assistData = ASSISTS[assistSkill.name];
             // todo pour plus tard : ajouter des "onAssist" aux skills qui s'activent après un assist (pour les trucs du genre Snare, le B de Mordecai, etc.)
             assistData.onApply.call(assistSkill, this.state, assisted);
             const newAssistingHealth = getNewHealth(assisting);
             const newAssistedHealth = getNewHealth(assisted);
-            assisted.getOne("Stats").update({
-                hp: 20
-            });
+            if (assisting.getOne("PreviewAssist")) {
+                assisting.addComponent({
+                    type: "PreviewHP",
+                    value: newAssistingHealth,
+                });
+
+                assisted.addComponent({
+                    type: "PreviewHP",
+                    value: newAssistedHealth,
+                });
+            } else {
+                this.world.runSystems("hp-mod");
+                this.world.runSystems("move");
+            }
         }
     }
 };
